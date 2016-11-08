65e1ac492d5d95a3612b04e012c816b3c3989cfc
Orlo Wang <ow.cc@outlook.com>
2016-11-08 15:58:17 +0800

    :squirrel: use apollo server

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,15 @@
 import * as express from 'express';
 import * as path from 'path';
-import * as graphqlHTTP from 'express-graphql';
+import { 
+  graphqlExpress,
+  graphiqlExpress
+ } from 'graphql-server-express';
 import { maskErrors } from 'graphql-errors';
 import * as morgan from 'morgan';
 import * as chalk from "chalk";
 import * as cors from 'cors';
 import * as favicon from 'serve-favicon';
+import * as bodyParser from 'body-parser';
 import schema from './schema';
 import loaders from './loaders';
 
@@ -28,25 +32,18 @@ let corsOptionsDelegate = function(req, callback){
 
 maskErrors(schema);
 
-graphQLServer.use((req, res, next) => {
-  res.set('Content-Type', 'application/graphql; charset=utf-16');
-  next();
-})
+// graphQLServer.use((req, res, next) => {
+//   res.set('Content-Type', 'application/graphql; charset=utf-16');
+//   next();
+// })
 graphQLServer.use(morgan('combined'));
 graphQLServer.use(favicon(path.join(__dirname, '../env/favicon.svg')));
 graphQLServer.use(cors(corsOptionsDelegate));
-graphQLServer.use(graphqlHTTP(req => {
-  return {
-    context: { loaders },
-    schema,
-    graphiql: true,
-    pretty: true,
-    formatError: error => ({
-      message: error.message,
-      locations: error.locations,
-      stack: error.stack
-    })
-  }
+graphQLServer.use('/graphql', bodyParser.json(), graphqlExpress({
+  schema: schema
+}));
+graphQLServer.use('/graphiql', graphiqlExpress({
+  endpointURL: '/graphql',
 }));
 
 graphQLServer.listen(env.port, () => console.log(
